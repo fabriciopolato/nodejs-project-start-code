918f48095e2de0fb15143f18e7fbad3ec9a90982
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-05-04 09:56:01 -0300

    done

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
+import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import './database';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
